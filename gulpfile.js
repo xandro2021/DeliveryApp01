eac31d1b02f79e704a2245c3cb777e073946c0f3
xandro2021 <xandrocastro2021@gmail.com>
2022-12-20 22:06:23 -0600

    nuevo gulp file

--- gulpfile.js
+++ gulpfile.js
@@ -1,60 +1,73 @@
-const { src, dest, watch, series } = require('gulp');
+const { src, dest, watch, series, parallel } = require( "gulp" );
 
 // CSS y SASS
-const sass = require('gulp-sass')(require('sass'));
-const postcss = require('gulp-postcss');
-const autoprefixer = require('autoprefixer');
-const sourcemaps = require('gulp-sourcemaps');
-const cssnano = require('cssnano');
+const sass = require( "gulp-sass" )( require( "sass" ) );
+const postcss = require( "gulp-postcss" );
+const autoprefixer = require( "autoprefixer" );
+const sourcemaps = require( "gulp-sourcemaps" );
+const cssnano = require( "cssnano" );
 
-// Imagenes
-const imagemin = require('gulp-imagemin');
-const webp = require('gulp-webp');
-const avif = require('gulp-avif');
+// IMAGENES
+const imagemin = require( "gulp-imagemin" );
+const webp = require( "gulp-webp" );
+const avif = require( "gulp-avif" );
 
+// Funcion que compila y guarda los cambios en el archivo
 function css( done ) {
-    src('src/scss/app.scss')
-        .pipe( sourcemaps.init() )
-        .pipe( sass() )
-        .pipe( postcss([ autoprefixer(), cssnano() ]) )
-        .pipe( sourcemaps.write('.'))
-        .pipe( dest('build/css') )
+  // compilar sass
+  // pasos: 1-Identificar archivo, 2-Compilarla, 3-Guardar el .css
 
-    done();
+  src( "src/scss/app.scss" )
+    .pipe( sourcemaps.init() )
+    .pipe( sass() ) // {outputStyle: expanded}, compressed
+    .pipe( postcss( [autoprefixer(), cssnano()] ) )
+    .pipe( sourcemaps.write(".") )
+    .pipe( dest("build/css") );
+
+  done();
 }
 
-function imagenes() {
-    return src('src/img/**/*')
-        .pipe( imagemin({ optimizationLevel: 3 }) )
-        .pipe( dest('build/img') )
+function imagenes ( done ) {
+  src( "src/img/**/*" )
+    .pipe( imagemin({ optimizationLevel: 3 }) )
+    .pipe( dest( "build/img" ) );
+
+  done();
 }
 
-function versionWebp() {
-    const opciones = {
-        quality: 50
-    }
-    return src('src/img/**/*.{png,jpg}')
-        .pipe( webp( opciones ) )
-        .pipe( dest('build/img') )
+function versionWebp( done ) {
+  const opciones = {
+    quality: 50
+  }
+  src( "src/img/**/*.{png,jpg}" )
+    .pipe( webp( opciones ) )
+    .pipe( dest( "build/img" ) );
+
+  done();
 }
+
 function versionAvif() {
-    const opciones = {
-        quality: 50
-    }
-    return src('src/img/**/*.{png,jpg}')
-        .pipe( avif( opciones ) )
-        .pipe( dest('build/img'))
+  const opciones = {
+    quality: 50
+  }
+
+  return src( "src/img/**/*.{png,jpg}" )
+    .pipe( avif( opciones ) )
+    .pipe( dest("build/img") );
 }
 
+// Funcion que esta a la escucha de los cambios
 function dev() {
-    watch( 'src/scss/**/*.scss', css );
-    watch( 'src/img/**/*', imagenes );
+  watch( "src/scss/**/*.scss", css );
+  watch( "src/img/**/*", imagenes );
 }
-
 
 exports.css = css;
 exports.dev = dev;
 exports.imagenes = imagenes;
 exports.versionWebp = versionWebp;
 exports.versionAvif = versionAvif;
-exports.default = series( imagenes, versionWebp, versionAvif, css, dev  );
+exports.default = series( imagenes, versionWebp, versionAvif, css, dev );
+
+// Series - Se inicia una tarea y hasta que finaliza se inicia la siguiente
+// Parallel - Todas inician al mismo tiempo y se completan de forma diferente
